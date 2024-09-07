587f27f1a1460108b8ecbffe0265ffff6d35bd67
gechjs <gizachew980@gmail.com>
2024-09-07 03:46:01 -0700

    header and footer added

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from "react";
+import Home from "./pages/home/Home.jsx";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div className="App">hello world</div>
-    </>
+    <div className="App">
+      <Home />
+    </div>
   );
 }
 
